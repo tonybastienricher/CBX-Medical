451dffb26f3c10333e4a7dc50ace7153e78af563
Tony Richer <bonjour@tonyricher.fr>
2023-07-15 14:42:13 +0200

    :sparkles: Apprendre carte stack

--- assets/script_card_stack.js
+++ assets/script_card_stack.js
@@ -1,39 +1,40 @@
 // Utility function
-function Util () {};
+function Util() {
+};
 
-/* 
+/*
 	class manipulation functions
 */
 Util.hasClass = function(el, className) {
-	if (el.classList) return el.classList.contains(className);
-	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+  if (el.classList) return el.classList.contains(className);
+  else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
 };
 
 Util.removeClass = function(el, className) {
-	var classList = className.split(' ');
-	if (el.classList) el.classList.remove(classList[0]);	
-	else if(Util.hasClass(el, classList[0])) {
-		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
-		el.className=el.className.replace(reg, ' ');
-	}
-	if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+  var classList = className.split(' ');
+  if (el.classList) el.classList.remove(classList[0]);
+  else if (Util.hasClass(el, classList[0])) {
+    var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+    el.className = el.className.replace(reg, ' ');
+  }
+  if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
 };
 
-/* 
+/*
   DOM manipulation
 */
 
 Util.is = function(elem, selector) {
-  if(selector.nodeType){
+  if (selector.nodeType) {
     return elem === selector;
   }
 
-  var qa = (typeof(selector) === 'string' ? document.querySelectorAll(selector) : selector),
+  var qa = (typeof (selector) === 'string' ? document.querySelectorAll(selector) : selector),
     length = qa.length,
     returnArr = [];
 
-  while(length--){
-    if(qa[length] === elem){
+  while (length--) {
+    if (qa[length] === elem) {
       return true;
     }
   }
@@ -41,68 +42,68 @@ Util.is = function(elem, selector) {
   return false;
 };
 
-/* 
+/*
 	Animate height of an element
 */
 Util.setHeight = function(start, to, element, duration, cb) {
-	var change = to - start,
-	    currentTime = null;
+  var change = to - start,
+    currentTime = null;
 
-  var animateHeight = function(timestamp){  
-    if (!currentTime) currentTime = timestamp;         
+  var animateHeight = function(timestamp) {
+    if (!currentTime) currentTime = timestamp;
     var progress = timestamp - currentTime;
-    var val = parseInt((progress/duration)*change + start);
-    element.style.height = val+"px";
-    if(progress < duration) {
-        window.requestAnimationFrame(animateHeight);
+    var val = parseInt((progress / duration) * change + start);
+    element.style.height = val + 'px';
+    if (progress < duration) {
+      window.requestAnimationFrame(animateHeight);
     } else {
-    	cb();
+      cb();
     }
   };
-  
+
   //set the height of the element before starting animation -> fix bug on Safari
-  element.style.height = start+"px";
+  element.style.height = start + 'px';
   window.requestAnimationFrame(animateHeight);
 };
 
 
 // Check if Reduced Motion is enabled
 Util.osHasReducedMotion = function() {
-  if(!window.matchMedia) return false;
+  if (!window.matchMedia) return false;
   var matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)');
-  if(matchMediaObj) return matchMediaObj.matches;
+  if (matchMediaObj) return matchMediaObj.matches;
   return false; // return false if not supported
-}; 
+};
 
-/* 
+/*
 	Polyfills
 */
 //Closest() method
 if (!Element.prototype.matches) {
-	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+  Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
 }
 
 if (!Element.prototype.closest) {
-	Element.prototype.closest = function(s) {
-		var el = this;
-		if (!document.documentElement.contains(el)) return null;
-		do {
-			if (el.matches(s)) return el;
-			el = el.parentElement || el.parentNode;
-		} while (el !== null && el.nodeType === 1); 
-		return null;
-	};
+  Element.prototype.closest = function(s) {
+    var el = this;
+    if (!document.documentElement.contains(el)) return null;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
 }
 
 //Custom Event() constructor
-if ( typeof window.CustomEvent !== "function" ) {
+if (typeof window.CustomEvent !== 'function') {
 
-  function CustomEvent ( event, params ) {
+  function CustomEvent(event, params) {
     params = params || { bubbles: false, cancelable: false, detail: undefined };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
     return evt;
-   }
+  }
 
   CustomEvent.prototype = window.Event.prototype;
 
@@ -110,23 +111,24 @@ if ( typeof window.CustomEvent !== "function" ) {
 }
 
 
-/* 
+/*
 	Animation curves
 */
-Math.easeInOutQuad = function (t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t + b;
-	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
+Math.easeInOutQuad = function(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t + b;
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
 };
 
 
 /* JS Utility Classes */
 (function() {
-  // make focus ring visible only for keyboard navigation (i.e., tab key) 
+  // make focus ring visible only for keyboard navigation (i.e., tab key)
   var focusTab = document.getElementsByClassName('js-tab-focus');
+
   function detectClick() {
-    if(focusTab.length > 0) {
+    if (focusTab.length > 0) {
       resetFocusTabs(false);
       window.addEventListener('keydown', detectTab);
     }
@@ -134,7 +136,7 @@ Math.easeInOutQuad = function (t, b, c, d) {
   };
 
   function detectTab(event) {
-    if(event.keyCode !== 9) return;
+    if (event.keyCode !== 9) return;
     resetFocusTabs(true);
     window.removeEventListener('keydown', detectTab);
     window.addEventListener('mousedown', detectClick);
@@ -142,7 +144,7 @@ Math.easeInOutQuad = function (t, b, c, d) {
 
   function resetFocusTabs(bool) {
     var outlineStyle = bool ? '' : 'none';
-    for(var i = 0; i < focusTab.length; i++) {
+    for (var i = 0; i < focusTab.length; i++) {
       focusTab[i].style.setProperty('outline', outlineStyle);
     }
   };
@@ -155,41 +157,43 @@ Math.easeInOutQuad = function (t, b, c, d) {
     this.items = this.element.getElementsByClassName('js-stack-cards__item');
     this.scrollingFn = false;
     this.scrolling = false;
-    initStackCardsEffect(this); 
-    initStackCardsResize(this); 
+    initStackCardsEffect(this);
+    initStackCardsResize(this);
   };
 
   function initStackCardsEffect(element) { // use Intersection Observer to trigger animation
     setStackCards(element); // store cards CSS properties
-		var observer = new IntersectionObserver(stackCardsCallback.bind(element), { threshold: [0, 1] });
-		observer.observe(element.element);
+    var observer = new IntersectionObserver(stackCardsCallback.bind(element), { threshold: [0, 1] });
+    observer.observe(element.element);
   };
 
   function initStackCardsResize(element) { // detect resize to reset gallery
-    element.element.addEventListener('resize-stack-cards', function(){
-      setStackCards(element);
-      animateStackCards.bind(element);
+    element.element.addEventListener('resize-stack-cards', function() {
+      if (window.innerWidth > 1024) {
+        setStackCards(element);
+        animateStackCards.bind(element);
+      }
     });
   };
-  
+
   function stackCardsCallback(entries) { // Intersection Observer callback
-    if(entries[0].isIntersecting) {
-      if(this.scrollingFn) return; // listener for scroll event already added
+    if (entries[0].isIntersecting) {
+      if (this.scrollingFn) return; // listener for scroll event already added
       stackCardsInitEvent(this);
     } else {
-      if(!this.scrollingFn) return; // listener for scroll event already removed
+      if (!this.scrollingFn) return; // listener for scroll event already removed
       window.removeEventListener('scroll', this.scrollingFn);
       this.scrollingFn = false;
     }
   };
-  
+
   function stackCardsInitEvent(element) {
     element.scrollingFn = stackCardsScrolling.bind(element);
     window.addEventListener('scroll', element.scrollingFn);
   };
 
   function stackCardsScrolling() {
-    if(this.scrolling) return;
+    if (this.scrolling) return;
     this.scrolling = true;
     window.requestAnimationFrame(animateStackCards.bind(this));
   };
@@ -209,49 +213,49 @@ Math.easeInOutQuad = function (t, b, c, d) {
     element.windowHeight = window.innerHeight;
 
     // reset margin + translate values
-    if(isNaN(element.marginY)) {
+    if (isNaN(element.marginY)) {
       element.element.style.paddingBottom = '0px';
     } else {
-      element.element.style.paddingBottom = (element.marginY*(element.items.length - 1))+'px';
+      element.element.style.paddingBottom = (element.marginY * (element.items.length - 1)) + 'px';
     }
 
-    for(var i = 0; i < element.items.length; i++) {
-      if(isNaN(element.marginY)) {
+    for (var i = 0; i < element.items.length; i++) {
+      if (isNaN(element.marginY)) {
         element.items[i].style.transform = 'none;';
       } else {
-        element.items[i].style.transform = 'translateY('+element.marginY*i+'px)';
+        element.items[i].style.transform = 'translateY(' + element.marginY * i + 'px)';
       }
     }
   };
 
   function getIntegerFromProperty(element) {
     var node = document.createElement('div');
-    node.setAttribute('style', 'opacity:0; visbility: hidden;position: absolute; height:'+element.marginY);
+    node.setAttribute('style', 'opacity:0; visbility: hidden;position: absolute; height:' + element.marginY);
     element.element.appendChild(node);
     element.marginY = parseInt(getComputedStyle(node).getPropertyValue('height'));
     element.element.removeChild(node);
   };
 
   function animateStackCards() {
-    if(isNaN(this.marginY)) { // --stack-cards-gap not defined - do not trigger the effect
-      this.scrolling = false;
-      return; 
-    }
-
-    var top = this.element.getBoundingClientRect().top;
-
-    if( this.cardTop - top + this.element.windowHeight - this.elementHeight - this.cardHeight + this.marginY + this.marginY*this.items.length > 0) { 
+    if (isNaN(this.marginY)) { // --stack-cards-gap not defined - do not trigger the effect
       this.scrolling = false;
       return;
     }
 
-    for(var i = 0; i < this.items.length; i++) { // use only scale
-      var scrolling = this.cardTop - top - i*(this.cardHeight+this.marginY);
-      if(scrolling > 0) {  
-        var scaling = i == this.items.length - 1 ? 1 : (this.cardHeight - scrolling*0.05)/this.cardHeight;
-        this.items[i].style.transform = 'translateY('+this.marginY*i+'px) scale('+scaling+')';
+    var top = this.element.getBoundingClientRect().top;
+
+    if (this.cardTop - top + this.element.windowHeight - this.elementHeight - this.cardHeight + this.marginY + this.marginY * this.items.length > 0) {
+      this.scrolling = false;
+      return;
+    }
+
+    for (var i = 0; i < this.items.length; i++) { // use only scale
+      var scrolling = this.cardTop - top - i * (this.cardHeight + this.marginY);
+      if (scrolling > 0) {
+        var scaling = i == this.items.length - 1 ? 1 : (this.cardHeight - scrolling * 0.05) / this.cardHeight;
+        this.items[i].style.transform = 'translateY(' + this.marginY * i + 'px) scale(' + scaling + ')';
       } else {
-        this.items[i].style.transform = 'translateY('+this.marginY*i+'px)';
+        this.items[i].style.transform = 'translateY(' + this.marginY * i + 'px)';
       }
     }
 
@@ -262,27 +266,30 @@ Math.easeInOutQuad = function (t, b, c, d) {
   var stackCards = document.getElementsByClassName('js-stack-cards'),
     intersectionObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype),
     reducedMotion = Util.osHasReducedMotion();
-    
-	if(stackCards.length > 0 && intersectionObserverSupported && !reducedMotion) { 
+
+  if (window.innerWidth > 1024 && stackCards.length > 0 && intersectionObserverSupported && !reducedMotion) {
     var stackCardsArray = [];
-		for(var i = 0; i < stackCards.length; i++) {
-			(function(i){
+    for (var i = 0; i < stackCards.length; i++) {
+      (function(i) {
         stackCardsArray.push(new StackCards(stackCards[i]));
       })(i);
     }
-    
+
     var resizingId = false,
       customEvent = new CustomEvent('resize-stack-cards');
-    
+
     window.addEventListener('resize', function() {
       clearTimeout(resizingId);
       resizingId = setTimeout(doneResizing, 500);
     });
 
     function doneResizing() {
-      for( var i = 0; i < stackCardsArray.length; i++) {
-        (function(i){stackCardsArray[i].element.dispatchEvent(customEvent)})(i);
-      };
+      for (var i = 0; i < stackCardsArray.length; i++) {
+        (function(i) {
+          stackCardsArray[i].element.dispatchEvent(customEvent);
+        })(i);
+      }
+      ;
     };
-	}
+  }
 }());
